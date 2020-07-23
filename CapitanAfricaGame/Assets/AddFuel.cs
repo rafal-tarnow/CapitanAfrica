fa072b74f3f26984d6662878ed06bd9b8faa2bef
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2020-07-23 21:44:31 +0200

    dziala tankowanie

---
+++ CapitanAfricaGame/Assets/AddFuel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddFuel : MonoBehaviour
+{
+    public CarController carController;
+    private void OnTriggerEnter2D(Collider2D other) {
+        carController.fuel = 1.0f;
+        Destroy(gameObject);
+    }
+}
--- CapitanAfricaGame/Assets/CarController.cs
+++ CapitanAfricaGame/Assets/CarController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float fuel = 1.0f;
+    public float fuelconsumption = 0.1f;
     public Rigidbody2D carRigidbody;
     public Rigidbody2D backTire;
     public Rigidbody2D frontTire;
     public float speed = 20;
     public float carTorque = 10;
     private float movement;
+    public UnityEngine.UI.Image image;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +23,16 @@ public class CarController : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
     }
 
     private void FixedUpdate() {
-        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-        carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        if(fuelconsumption > 0.0f)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
+        fuel -= fuelconsumption*Mathf.Abs(movement)*Time.fixedDeltaTime;
     }
 }
