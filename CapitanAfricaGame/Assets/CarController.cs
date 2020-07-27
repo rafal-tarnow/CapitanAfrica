63cfc5cae48f212306a601873de92e1002cbdce8
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2020-07-27 11:51:36 +0200

    poprawki

--- CapitanAfricaGame/Assets/CarController.cs
+++ CapitanAfricaGame/Assets/CarController.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float fuel = 1.0f;
+    public float fuel = 0.5f;
     public float fuelconsumption = 0.1f;
     public Rigidbody2D carRigidbody;
-    public Rigidbody2D backTire;
-    public Rigidbody2D frontTire;
     public float speed = 20;
     public float carTorque = 10;
     private float movement;
@@ -34,28 +32,57 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(Input.touchCount > 0) //Touch
-        // {
-        //     Touch touch = Input.GetTouch(0);
-
-        //     if(touch.position.x > (Screen.width/2))
-        //     {
-        //         movement = 1.0f;
-        //     }
-        //     else
-        //     {
-        //         movement = -1.0f;
-        //     }
-        // }
-        // else    //Keyboard
-        // {
-        //     movement = Input.GetAxis("Horizontal");
-        // }
-
-        float x = getGas();
+        //FUEL
+        image.fillAmount = fuel;
+        //DISTANCE
+        DistanceText.distance = Mathf.CeilToInt(transform.position.x);
+    }
 
 
-        if(x > 0)
+
+    void FixedUpdate() 
+    {
+        float gas = getGas();
+        
+        if(fuel > 0.0f)
+        {
+            applyGasToCar(gas);
+
+            //carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+            fuel -= fuelconsumption*Mathf.Abs(gas)*Time.fixedDeltaTime;
+        }
+        else
+        {
+            applyGasToCar(0);
+        }
+        
+    }    
+    
+    private float getGas()
+    {
+
+        if(Input.touchCount > 0) //Touch
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.position.x > (Screen.width/2))
+            {
+                return(1.0f);
+            }
+            else
+            {
+                return (-1.0f);
+            }
+        }
+        else    //Keyboard
+        {
+            return(Input.GetAxis("Horizontal"));
+        }
+    }
+
+    private void applyGasToCar(float x)
+    {
+     if(x > 0)
         {
             backMotor.motorSpeed = speedForward;
             frontMotor.motorSpeed = speedForward;
@@ -89,42 +116,5 @@ public class CarController : MonoBehaviour
             wheelFrontJoint.motor = frontMotor;
             wheelBackJoint.motor = backMotor;                           
         }
-
-        //FUEL
-        image.fillAmount = fuel;
-        //DISTANCE
-        DistanceText.distance = Mathf.CeilToInt(transform.position.x);
-    }
-
-    private float getGas()
-    {
-
-        if(Input.touchCount > 0) //Touch
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if(touch.position.x > (Screen.width/2))
-            {
-                return(1.0f);
-            }
-            else
-            {
-                return (-1.0f);
-            }
-        }
-        else    //Keyboard
-        {
-            return(Input.GetAxis("Horizontal"));
-        }
-    }
-
-    private void FixedUpdate() {
-        if(fuelconsumption > 0.0f)
-        {
-            //backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-            //frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-            //carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
-        }
-        fuel -= fuelconsumption*Mathf.Abs(movement)*Time.fixedDeltaTime;
     }
 }
