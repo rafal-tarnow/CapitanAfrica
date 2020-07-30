99a1df627d73b3713e769afd85ec6ab8dc27a0b2
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2020-07-30 15:52:06 +0200

    przesuwanie monetki i paliwo

---
+++ CapitanAfricaGame/Assets/DragableSprite.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragableSprite : MonoBehaviour
+{
+    private float startPosX;
+    private float startPoxY;
+    private bool isBeginHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBeginHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPoxY, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Coin Mouse Down");
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPoxY = mousePos.y - this.transform.localPosition.y;
+
+            isBeginHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Coin Mouse Up");
+        isBeginHeld = false;
+    }
+}
