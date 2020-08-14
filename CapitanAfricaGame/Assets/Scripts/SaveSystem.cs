6c4296123eddd43afcd4e56e285e5ad3d5439c29
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2020-08-14 23:18:50 +0200

    dorobienie zapisywania leveli na sdcard

--- CapitanAfricaGame/Assets/Scripts/SaveSystem.cs
+++ CapitanAfricaGame/Assets/Scripts/SaveSystem.cs
@@ -13,7 +13,12 @@ public static class SaveSystem
 
         if (Application.platform == RuntimePlatform.Android)
         {
-            SAVE_FOLDER = Application.persistentDataPath;
+            SAVE_FOLDER = GetAndroidExternalFilesDir() + "/CapitanAfrica";
+            
+            if(!Directory.Exists(SAVE_FOLDER))
+            {
+                Directory.CreateDirectory(SAVE_FOLDER);
+            }
         }
         else
         {
@@ -46,6 +51,48 @@ public static class SaveSystem
         {
             return default(T);
         }
+    }
+
+    private static string GetAndroidExternalFilesDir()
+    {
+     using (AndroidJavaClass unityPlayer = 
+            new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+     {
+          using (AndroidJavaObject context = 
+                 unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+          {
+               // Get all available external file directories (emulated and sdCards)
+               AndroidJavaObject[] externalFilesDirectories = 
+                                   context.Call<AndroidJavaObject[]>
+                                   ("getExternalFilesDirs", (object)null);
+
+               AndroidJavaObject emulated = null;
+               AndroidJavaObject sdCard = null;
+
+               for (int i = 0; i < externalFilesDirectories.Length; i++)
+               {
+                    AndroidJavaObject directory = externalFilesDirectories[i];
+                    using (AndroidJavaClass environment = 
+                           new AndroidJavaClass("android.os.Environment"))
+                    {
+                        // Check which one is the emulated and which the sdCard.
+                        bool isRemovable = environment.CallStatic<bool>
+                                          ("isExternalStorageRemovable", directory);
+                        bool isEmulated = environment.CallStatic<bool>
+                                          ("isExternalStorageEmulated", directory);
+                        if (isEmulated)
+                            emulated = directory;
+                        else if (isRemovable && isEmulated == false)
+                            sdCard = directory;
+                    }
+               }
+               // Return the sdCard if available
+               if (sdCard != null)
+                    return sdCard.Call<string>("getAbsolutePath");
+               else
+                    return emulated.Call<string>("getAbsolutePath");
+            }
+      }
     }
 
 }
